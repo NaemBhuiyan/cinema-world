7ec35c8a719c23124ff489c4376901ca82ded68f
Naem Bhuiyan <32921299+NaemBhuiyan@users.noreply.github.com>
2022-05-14 04:44:07 +0600

    remove unnecessary file

--- src/components/AppProvider/index.js
+++ src/components/AppProvider/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ConfigProvider } from 'antd';
-import { IntlProvider } from 'react-intl';
 import PropTypes from 'prop-types';
 import { theme, AppLocale } from '@/config';
-import { LanguageStore } from '@/store';
 
 export default function AppProvider({ children }) {
-  const locale = LanguageStore(state => state.locale);
-  const currentAppLocale = AppLocale[locale];
+  const currentAppLocale = AppLocale['en'];
 
   return (
     <ConfigProvider locale={currentAppLocale.antd}>
-      <IntlProvider
-        locale={currentAppLocale.locale}
-        messages={currentAppLocale.messages}
-      >
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </IntlProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </ConfigProvider>
   );
 }
