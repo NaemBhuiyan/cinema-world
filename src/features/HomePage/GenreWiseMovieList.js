b3ae1b6917a8f8407ebdd7973a9dc2fd77961be3
Naem Bhuiyan <32921299+NaemBhuiyan@users.noreply.github.com>
2022-05-14 16:29:43 +0600

    API call if genre type in view

--- src/features/HomePage/GenreWiseMovieList.js
+++ src/features/HomePage/GenreWiseMovieList.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Genre } from '../genre/api';
-import { useQuery } from 'react-query';
+import { useInView } from 'react-intersection-observer';
 import MovieCard from '@/components/MovieCard';
 import { Col, Row } from 'antd';
 
 function GenreWiseMovieList({ genre }) {
-  const { data, isLoading } = useQuery(['catagories-movie-list', genre], () =>
-    Genre.genreWiseMovieList(genre),
-  );
+  const [data, setData] = useState();
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
+  useEffect(async () => {
+    if (inView) {
+      const res = await Genre.genreWiseMovieList(genre);
+      if (res.length) {
+        setData(res);
+      }
+    }
+  }, [inView]);
 
   if (data?.length) {
     return (
-      <Row gutter={30} className="mb-5">
+      <Row gutter={30} className="mb-5" ref={ref} key={genre.id}>
         {data.slice(0, 5).map(movie => (
           <Col key={movie.id} span={5} className="my-3">
-            <MovieCard movie={movie} isLoading={isLoading} />
+            <MovieCard movie={movie} isLoading={false} />
           </Col>
         ))}
       </Row>
     );
   }
-  return 'Data not found';
-  // return (
-  //   movieList?.data?.length &&
-  //   movieList?.data?.map(movie => <MovieCard key={movie.id} movie={movie} />)
-  // );
+  return <p ref={ref}>Data not found</p>;
 }
 
 GenreWiseMovieList.propTypes = {
