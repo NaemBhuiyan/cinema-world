57e2d041d5fc20059b8f9a68fc699bfd811bc68c
Naem Bhuiyan <32921299+NaemBhuiyan@users.noreply.github.com>
2022-05-18 16:49:37 +0600

    trying to implement test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, test, expect } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+// import regeneratorRuntime from 'regenerator-runtime';
+test('renders learn react link', async () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
